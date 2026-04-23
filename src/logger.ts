e7b5fc622068fa8afe3d92ec00161b0345ded108
Lucas-Delacroix <delacroixdev@gmail.com>
2026-04-23 15:28:33 -0300

    chore: change log level

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,6 @@
 import pino from 'pino';
 
-const isPrettyEnabled = process.env.NODE_ENV === 'development';
+const isPrettyEnabled = process.env.NODE_ENV !== 'production';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? 'info',

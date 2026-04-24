d5f46e98be5d726ac3b63279fe30c163350a4abf
Lucas-Delacroix <delacroixdev@gmail.com>
2026-04-24 20:01:52 -0300

    build: setup pnpm config files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      all: true,
       enabled: false,
+      exclude: ['dist/**', 'scripts/**', 'test/**'],
+      include: ['src/**/*.ts'],
+      provider: 'v8',
+      reporter: ['text', 'json-summary', 'html', 'lcov'],
+      reportsDirectory: './coverage',
     },
     environment: 'node',
     include: ['test/**/*.test.ts'],
